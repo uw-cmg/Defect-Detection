ea861444ce2cecdab3de975a6732e1052255f3cc
leewaymay <leewaymay@gmail.com>
2018-02-26 16:49:44 -0600

    update readme

--- utils/DefectDetectionDataset.py
+++ utils/DefectDetectionDataset.py
@@ -4,10 +4,10 @@ import os
 import chainer
 from chainercv import utils
 
-root = '/home/wei/'
+root = '/home/wei/Data/Loop_detection'
 
 def get_dir():
-    return os.path.join(root, 'data')
+    return root
 
 class DefectDetectionDataset(chainer.dataset.DatasetMixin):
     """Base class for defect defection dataset
@@ -19,12 +19,9 @@ class DefectDetectionDataset(chainer.dataset.DatasetMixin):
 
         self.data_dir = data_dir
         images_file = os.path.join(data_dir, 'images.txt')
-        bbs_file = os.path.join(data_dir, 'bounding_boxes.txt')
-        image_class_labels_file = os.path.join(
-            data_dir, 'image_class_labels.txt')
 
-        self.paths = [
-            line.strip().split()[1] for line in open(images_file)]
+        self.images = [
+            line.strip() for line in open(images_file)]
 
         # bbs should be a matrix (m by 4). m is the number of bounding
         # boxes in the image
@@ -45,7 +42,7 @@ class DefectDetectionDataset(chainer.dataset.DatasetMixin):
         self._labels = np.array(labels, dtype=np.int32)
 
     def __len__(self):
-        return len(self.paths)
+        return len(self.images)
 
     def get_example(self, i):
         """Returns the i-th example.
@@ -53,15 +50,17 @@ class DefectDetectionDataset(chainer.dataset.DatasetMixin):
         Args:
             i (int): The index of the example.
 
-        Returns:
+        Returns:s
             tuple of an image and its label.
             The image is in CHW format and its color channel is ordered in
             RGB.
             a bounding box is appended to the returned value.
         """
         img = utils.read_image(
-            os.path.join(self.data_dir, 'images', self.paths[i]),
-            color=True)
-        label = self._labels[i]
+            os.path.join(self.data_dir, 'images', self.images[i]),
+            color=False)
+        bbs_file = os.path.join(self.data_dir, 'bounding_boxes', self.images[i][0:-4]+'.txt')
+        bbs = np.stack([line.strip().split() for line in open(bbs_file)]).astype(np.float32)
+        label = np.stack([1]*bbs.shape[0]).astype(np.int32)
 
-        return img, label, self.bbs[i]
+        return img, bbs, label
